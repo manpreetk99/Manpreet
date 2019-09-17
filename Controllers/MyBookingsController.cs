eb863ac05e5338e22d9316976dcc989fa602dc10
Scott Klein <Scotty.klei@gmail.com>
2019-09-17 19:38:18 +1000

    All customer views and controllers implemented

--- Controllers/MyBookingsController.cs
+++ Controllers/MyBookingsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using BadgerysCreekHotel.Data;
 using Microsoft.AspNetCore.Authorization;
@@ -16,9 +17,21 @@ namespace BadgerysCreekHotel.Controllers
         {
             _context = context;
         }
-        public IActionResult Bookings()
+        public async Task<IActionResult> Bookings()
         {
-            var bookings = _context.Booking.ToList();
+            string customer = User.FindFirst(ClaimTypes.Name).Value; // get logged in user name
+            //var bookings = _context.Booking.Where(.ToList();
+            var bookings =
+                from booking in _context.Booking
+                where booking.CustomerEmail == customer
+                select booking;
+
+            //ensure that the bookings associate with the customer
+            foreach (var booking in bookings)
+            {
+                var user = await _context.Customer.FindAsync(customer);
+                booking.TheCustomer = user;
+            }
             return View(bookings);
         }
     }
