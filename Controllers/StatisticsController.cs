b30650f002e5f88b2c1fdd70d5b14ba94f99c865
Scott Klein <scotty.klei@gmail.com>
2019-09-18 13:27:09 +1000

    Admin statistics functional untested

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -2,17 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BadgerysCreekHotel.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.EntityFrameworkCore;
+using BadgerysCreekHotel.ViewModels;
 namespace BadgerysCreekHotel.Controllers
 {
+
     [Authorize(Roles = "Admin")]
     public class StatisticsController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public StatisticsController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Statistics()
+        {
+            var customers =
+                from customer in _context.Customer
+                group customer by customer.PostCode into g
+                select new CustomerStat
+                {
+                    PostCode = g.Key,
+                    Count = g.Count()
+                };
+
+            var rooms =
+                from room in _context.Booking
+                group room by room.RoomID into g
+                select new RoomStat
+                {
+                    RoomID = g.Key,
+                    Count = g.Count()
+                };
+            var customerStats = await customers.ToListAsync();
+            var roomStats = await rooms.ToListAsync();
+
+            StatView statView = new StatView
+            {
+                CustomerStats = customerStats,
+                RoomStats = roomStats
+            };
+
+            return View(statView);
         }
     }
 }
---
+++ ViewModels/StatView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BadgerysCreekHotel.ViewModels
+{
+    public class StatView
+    {
+        public List<CustomerStat> CustomerStats { get; set; }
+        public List<RoomStat> RoomStats { get; set; }
+    }
+
+    public class CustomerStat
+    {
+        public int Count { get; set; }
+        public string PostCode { get; set; }
+    }
+
+    public class RoomStat
+    {
+        public int Count { get; set; }
+        public int RoomID { get; set; }
+    }
+}
